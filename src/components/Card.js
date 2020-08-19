3f4eeea24da9828e423a23b86767a73d70410dc6
neelkim <neelkim1989@gmail.com>
2020-08-19 16:13:30 +0500

    использовать контекст CurrentUserContext в компоненте Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Card(props) {
+  const currentUser = useContext(CurrentUserContext);
+  
+  // Определяем, являемся ли мы владельцем текущей карточки
+  const isOwn = props.card.owner._id === currentUser._id;
+
+  // Создаём переменную, которую после зададим в `className` для кнопки удаления
+  const cardDeleteButtonClassName = (
+    `element__remove ${isOwn && 'element__remove_visible'}`
+  );
+
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+
+  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = `element__like ${isLiked && element__like_liked}`;
+
   return (
     <figure className="element">
       <div className="element__image-container">
@@ -16,11 +33,11 @@ function Card(props) {
       <div className="element__info-container">
         <h2 className="element__name">{props.card.name}</h2>
         <div className="element__like-container">
-          <button className="element__like" type="button" aria-label="Нравится"/>
+          <button className={cardLikeButtonClassName} type="button" aria-label="Нравится"/>
           <p className="element__like-counter">{props.card.likes.length}</p>
         </div>
       </div>
-      <button className="element__remove" aria-label="Удалить"/>
+      <button className={cardDeleteButtonClassName} aria-label="Удалить"/>
     </figure>
   );
 }
